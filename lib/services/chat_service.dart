ff65a10a0abb23ab5e3ef657cd950494bebe7902
JAMIL AHMAD <jamilmedia099@gmail.com>
2026-08-01 11:12:40 +0600

    Add ChatService class with sendMessage and getMessages

---
+++ lib/services/chat_service.dart
@@ -0,0 +1,9 @@
+class ChatService {
+  Future<void> sendMessage(String message) async {
+    // পরে Supabase Chat যুক্ত হবে
+  }
+
+  Future<List<String>> getMessages() async {
+    return [];
+  }
+}
