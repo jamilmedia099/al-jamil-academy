6818747bf8ac8902e07fba06372e335857841788
JAMIL AHMAD <jamilmedia099@gmail.com>
2026-08-01 11:11:53 +0600

    Create auth_service.dart

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,19 @@
+class AuthService {
+  Future<bool> login(String email, String password) async {
+    // পরে Supabase Login এখানে যুক্ত হবে
+    return true;
+  }
+
+  Future<bool> register(
+    String name,
+    String email,
+    String password,
+  ) async {
+    // পরে Supabase Register এখানে যুক্ত হবে
+    return true;
+  }
+
+  Future<void> logout() async {
+    // পরে Logout যুক্ত হবে
+  }
+}
